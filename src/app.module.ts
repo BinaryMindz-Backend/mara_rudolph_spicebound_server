e004d3768804d0a66801189636d706f24fc1855a
Rahman Abdul Quadir <rahmanaq777@gmail.com>
2026-01-14 00:40:48 +0600

    prisma module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { AuthModule } from './main/auth/auth.module.js';
+import { PrismaModule } from './main/prisma/prisma.module.js';
+
 
 
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main/auth/auth.module.ts
+++ src/main/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller.js';
 import { AuthService } from './auth.service.js';
+import { JwtStrategy } from './strategies/jwt.strategy.js';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
