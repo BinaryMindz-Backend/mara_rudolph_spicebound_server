63378dc5ee275e661ed7181b50f6f46393a0e795
Rahman Abdul Quadir <rahmanaq777@gmail.com>
2026-01-15 00:17:00 +0600

    swagger configured

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   // 🔐 Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Spicebound API')
+    .setDescription('Backend API for Spicebound MVP')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
